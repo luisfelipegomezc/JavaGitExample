cb6c7266754d9bc9f8db81c078a593aefec12c56
FELIPE <FELIPE@FELIPE_DOS>
2016-02-18 19:02:16 -0500

    versión 1.0 Datos Versión de prueba de conexión GitHub

--- src/javagitexample/JavaGitExample.java
+++ src/javagitexample/JavaGitExample.java
@@ -15,7 +15,7 @@ public class JavaGitExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("ensayo de repositorio");
     }
     
 }
