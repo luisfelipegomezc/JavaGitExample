bdb6a68204c1117365de5fa454c5e18f12dc9d72
FELIPE <FELIPE@FELIPE_DOS>
2016-02-24 02:04:40 -0500

    versión 1.0 Datos Versión de prueba de conexión GitHub

---
+++ src/javagitexample/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagitexample;
+import java.util.Scanner;
+
+public class Usuario {
+    public String nombre;
+    public String apellido;
+    public String cedula;
+    
+    
+}
