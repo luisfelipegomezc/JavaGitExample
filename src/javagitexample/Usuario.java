fde0307b0de8dc0565c43ad9d6466df7670335c0
FELIPE <FELIPE@FELIPE_DOS>
2016-02-24 11:39:57 -0500

    versión 1.0 Datos Versión de prueba de conexión GitHub

--- src/javagitexample/Usuario.java
+++ src/javagitexample/Usuario.java
@@ -11,5 +11,33 @@ public class Usuario {
     public String apellido;
     public String cedula;
     
+    private String nombre1;
+    private String apellido1;
+    private String cedula1;
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getCedula1() {
+        return cedula1;
+    }
+
+    public void setCedula1(String cedula1) {
+        this.cedula1 = cedula1;
+    }
+    
     
 }
